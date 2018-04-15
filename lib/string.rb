e3be5f449840b0b737deab673676b914f6215d16
QuiLT <quilt@atmarkcafe.org>
2018-04-15 19:29:06 +0700

    refactor

--- lib/string.rb
+++ lib/string.rb
@@ -1,8 +1,5 @@
-KANA_TO_ROM = { 1=>"一", 2=>"二", 3=>"三", 4=>"四", 5=>"五",6=>"六", 7=>"七", 8=>"八", 9=>"九",10=>"十",100=>"百",
-   (10 **3) =>"千" ,(10 ** 4)=>"万", (10 ** 8)=>"億", (10 ** 12)=>"兆",
-   (10 ** 16)=> "京",(10 ** 20)=>"垓",(10 ** 24)=>"秭",(10 ** 28)=> "穣", (10 ** 32)=>"溝",
-   (10 ** 36)=>"澗", (10 ** 40)=>"正",(10 ** 44)=>"載"}
-KEY = ['万','億','兆','京','垓','秭','穣','溝','澗','正','載'] 
+KANA_TO_ROM = { 1=>"一", 2=>"二", 3=>"三", 4=>"四", 5=>"五",6=>"六", 7=>"七", 8=>"八", 9=>"九",10=>"十",100=>"百",(10 **3) =>"千" ,(10 ** 4)=>"万", (10 ** 8)=>"億", (10 ** 12)=>"兆",(10 ** 16)=> "京",(10 ** 20)=>"垓",(10 ** 24)=>"秭",(10 ** 28)=> "穣", (10 ** 32)=>"溝",(10 ** 36)=>"澗", (10 ** 40)=>"正",(10 ** 44)=>"載", (10 ** 48) => "極"}
+KEY = ['万','億','兆','京','垓','秭','穣','溝','澗','正','載','極']
 class String
   def check
     KEY.each do |key|
@@ -10,7 +7,6 @@ class String
     end
     return true
   end
-
   def to_number
     result = 0
     return get_number(self) if check
@@ -42,38 +38,29 @@ class String
     return array.push(begin: (array.last[:end]+1), end: self.length)
   end
   def to_kansuji
-    return (kanji_to(self)) if self.length <= 5
+    return (kanji_to_demo(self)) if self.length <= 5
     @position_end = self.length
-    @array = []
-    analysic_refactor.reverse.each do |array|
-      kanji_to(array[:value])
-      @a += array[:key] if array[:backup].to_i > 0
+    result = ''
+    self.reverse.split('').each_slice(4).each.with_index do |fa, index|
+      @kan = ''
+      key = index > 0 ? KEY[index-1] : '' if fa.reverse.join('').to_i > 0 # defined?(result).nil? ? result=(kanji_to_demo(fa.reverse.join('')).to_s + key.to_s) : result=(kanji_to_demo(fa.reverse.join('')).to_s + key.to_s + result.to_s)
+      result=(kanji_to_demo(fa.reverse.join('')).to_s + key.to_s + result.to_s)
     end
-    return @a
+    return result
   end
-  def analysic_refactor
-    return @array.push(key: get_key(@array), value: self[0..(@position_end-1)], backup: self[0..(@position_end-1)]) if @position_end - 4 <= 0 # chia 4,4,4,4,
-    @array.push(key: get_key(@array), value: self[(@position_end - 4)..@position_end-1], backup: self[(@position_end - 4)..@position_end-1]) if @array.present?
-    @array.push(key: '', value: self[(@position_end - 4)..@position_end], backup: self[(@position_end - 4)..@position_end]) if @array.blank?
-    @position_end -= 4
-    analysic_refactor
-  end
-  def get_key(array)
-    return KEY[@array.size-1]
-  end
-  def kanji_to(string)
-    return @a.to_s if string.length == 0
+  def kanji_to_demo(string)
+    return @kan.to_s if string.length == 0
     if string[0] != '0'
-      return (@a = @a.to_s + KANA_TO_ROM[string[0].to_i].to_s) if string.length == 1 #why a.to_s ma @a='' but @a = nil
+      return (@kan = @kan.to_s + KANA_TO_ROM[string[0].to_i].to_s) if string.length == 1 #why a.to_s ma @kan='' but @kan = nil
       if string[0] == '1' && string.length >= 5 # duoi 10000 k co 一
-        @a = @a.to_s + '一'.to_s + KANA_TO_ROM[(string[0].to_i*10**(string.length-1))].to_s #xu ly so 1
+        @kan = @kan.to_s + '一'.to_s + KANA_TO_ROM[(string[0].to_i*10**(string.length-1))].to_s #xu ly so 1
       elsif string[0] == '1' #tren 10000 co so 一
-        @a = @a.to_s + KANA_TO_ROM[(string[0].to_i*10**(string.length-1))].to_s  #xu ly so 1
-      elsif string[0] != '1'
-        @a = @a.to_s + KANA_TO_ROM[string[0].to_i].to_s + KANA_TO_ROM[(10**(string.length-1))].to_s
+        @kan = @kan.to_s + KANA_TO_ROM[(string[0].to_i*10**(string.length-1))].to_s  #xu ly so 1
+      else
+        @kan = @kan.to_s + KANA_TO_ROM[string[0].to_i].to_s + KANA_TO_ROM[(10**(string.length-1))].to_s
       end
     end
     string.slice!(0)
-    kanji_to(string)
+    kanji_to_demo(string)
   end
 end
