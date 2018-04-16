6bffc6e12055718bb526163e956bbf0991c89c5d
QuiLT <quilt@atmarkcafe.org>
2018-04-16 12:18:15 +0700

    chia numeric & string

--- lib/string.rb
+++ lib/string.rb
@@ -1,27 +1,23 @@
-KANA_TO_ROM = { 1=>"一", 2=>"二", 3=>"三", 4=>"四", 5=>"五",6=>"六", 7=>"七", 8=>"八", 9=>"九",10=>"十",100=>"百",(10 **3) =>"千" ,(10 ** 4)=>"万", (10 ** 8)=>"億", (10 ** 12)=>"兆",(10 ** 16)=> "京",(10 ** 20)=>"垓",(10 ** 24)=>"秭",(10 ** 28)=> "穣", (10 ** 32)=>"溝",(10 ** 36)=>"澗", (10 ** 40)=>"正",(10 ** 44)=>"載", (10 ** 48) => "極"}
-KEY = ['万','億','兆','京','垓','秭','穣','溝','澗','正','載','極']
+require 'datatables'
 class String
-  def check
-    KEY.each do |key|
-      return false if self.index(key)
-    end
-    return true
-  end
   def to_number
     result = 0
-    return get_number(self) if check
-    hash_kanji_to.each do |array|
-      return result += get_number(self[array[:begin]..(array[:end])]).to_i  if hash_kanji_to.last == array
-      result += get_number(self[array[:end]]).to_i if array[:end] - array[:begin] == 0
-      result += get_number(self[array[:begin]..(array[:end]-1)]).to_i * get_number(self[array[:end]]).to_i if array[:end] - array[:begin] > 0
+    return get_number(self) if !KEY.any?{|key| self.include?(key)}
+    hash_kanji.each do |array|
+      return result += get_number(self[array[:begin]..(array[:end])]).to_i  if hash_kanji.last == array
+      if array[:end] - array[:begin] == 0
+        result += ROM_TO_KANJI[array[:key]].to_i
+      else
+        result += get_number(self[array[:begin]..(array[:end]-1)]).to_i * ROM_TO_KANJI[array[:key]].to_i # >0
+      end
     end
   end
   def get_number(string)
     result = 0
     while string.present? do
-      current = Hash[KANA_TO_ROM.to_a.map{|m| m.reverse}][string[0]].to_i
-      if Hash[KANA_TO_ROM.to_a.map{|m| m.reverse}][string[1]].to_i % 10 == 0 && string[1].present? && current % 10 != 0 #eample: 20,200,2020
-        current = current*Hash[KANA_TO_ROM.to_a.map{|m| m.reverse}][string[1]].to_i
+      current = ROM_TO_KANJI[string[0]].to_i
+      if ROM_TO_KANJI[string[1]].to_i % 10 == 0 && string[1].present? && current % 10 != 0 #eample: 20,200,2020
+        current = current*ROM_TO_KANJI[string[1]].to_i
         string.slice!(0)
       end
       result += current.to_i
@@ -29,38 +25,40 @@ class String
       string.slice!(0)
     end
   end
-  def hash_kanji_to
+  def hash_kanji
     array = []
     KEY.reverse.each do |item|
-      array.push(begin: 0, end: self.index(item)) if array.blank? && self.index(item).present?
-      array.push(begin: (array.last[:end]+1), end: self.index(item)) if array.present? && self.index(item).present? && (array.last[:end]+1) <= self.index(item)
+      array.push(begin: 0, end: self.index(item), key: item) if array.blank? && self.index(item).present?
+      array.push(begin: (array.last[:end]+array.last[:key].length), end: self.index(item), key: item) if array.present? && self.index(item).present? && (array.last[:end]+1) <= self.index(item)
     end
-    return array.push(begin: (array.last[:end]+1), end: self.length)
+    return array.push(begin: (array.last[:end]+array.last[:key].length), end: self.length, key:'')
   end
-  def to_kansuji
-    return (kanji_to_demo(self)) if self.length <= 5
-    @position_end = self.length
+end
+class Numeric
+  def to_kansuji()
+    return (kanji_to_demo(self.to_s)) if self.to_s.length <= 5
     result = ''
-    self.reverse.split('').each_slice(4).each.with_index do |fa, index|
-      @kan = ''
+    self.to_s.reverse.split('').each_slice(4).each.with_index do |fa, index|
       key = index > 0 ? KEY[index-1] : '' if fa.reverse.join('').to_i > 0 # defined?(result).nil? ? result=(kanji_to_demo(fa.reverse.join('')).to_s + key.to_s) : result=(kanji_to_demo(fa.reverse.join('')).to_s + key.to_s + result.to_s)
       result=(kanji_to_demo(fa.reverse.join('')).to_s + key.to_s + result.to_s)
     end
     return result
   end
   def kanji_to_demo(string)
-    return @kan.to_s if string.length == 0
-    if string[0] != '0'
-      return (@kan = @kan.to_s + KANA_TO_ROM[string[0].to_i].to_s) if string.length == 1 #why a.to_s ma @kan='' but @kan = nil
-      if string[0] == '1' && string.length >= 5 # duoi 10000 k co 一
-        @kan = @kan.to_s + '一'.to_s + KANA_TO_ROM[(string[0].to_i*10**(string.length-1))].to_s #xu ly so 1
-      elsif string[0] == '1' #tren 10000 co so 一
-        @kan = @kan.to_s + KANA_TO_ROM[(string[0].to_i*10**(string.length-1))].to_s  #xu ly so 1
-      else
-        @kan = @kan.to_s + KANA_TO_ROM[string[0].to_i].to_s + KANA_TO_ROM[(10**(string.length-1))].to_s
+    kan = ''
+    while string do
+      if string[0] != '0'
+        return (kan = kan.to_s + KANA_TO_ROM[string[0].to_i]) if string.length == 1 #why a.to_s ma @kan='' but @kan = nil
+        if string[0] == '1' && string.length >= 5 # duoi 10000 k co 一
+          kan = kan.to_s + '一'.to_s + KANA_TO_ROM[(string[0].to_i*10**(string.length-1))].to_s #xu ly so 1
+        elsif string[0] == '1' #tren 10000 co so 一
+          kan = kan.to_s + KANA_TO_ROM[(string[0].to_i*10**(string.length-1))].to_s  #xu ly so 1
+        else
+          kan = kan.to_s + KANA_TO_ROM[string[0].to_i].to_s + KANA_TO_ROM[(10**(string.length-1))].to_s
+        end
       end
+      string.slice!(0)
+      return kan if string.length == 0
     end
-    string.slice!(0)
-    kanji_to_demo(string)
   end
 end
--- spec/lib/kanji_to_number_spec.rb
+++ spec/lib/kanji_to_number_spec.rb
@@ -2,99 +2,143 @@ require 'rails_helper'
 describe Integer do
   context "should convert" do
       it "number to kanji 一" do
-        '一'.to_number().should == 1
+        '一'.to_number.should == 1
       end
       it "number to kanji 三" do
-        '三'.to_number().should == 3
+        '三'.to_number.should == 3
       end
       it "number to kanji 十" do
-        '十'.to_number().should == 10
+        '十'.to_number.should == 10
       end
       it "number to kanji 十三" do
-        '十三'.to_number().should == 13
+        '十三'.to_number.should == 13
       end
       it "number to kanji 二十" do
-        '二十'.to_number().should == 20
+        '二十'.to_number.should == 20
       end
       it "number to kanji 十二" do
-        '十二'.to_number().should == 12
+        '十二'.to_number.should == 12
       end
       it "number to kanji 百" do
-        '百'.to_number().should == 100
+        '百'.to_number.should == 100
       end
       it "number to kanji 百五" do
-        '百五'.to_number().should == 105
+        '百五'.to_number.should == 105
       end
       it "number to kanji 百三十五" do
-        '百三十五'.to_number().should == 135
+        '百三十五'.to_number.should == 135
       end
       it "number to kanji 千" do
-        '千'.to_number().should == 1000
+        '千'.to_number.should == 1000
       end
       it "number to kanji 千十一" do
-        '千十一'.to_number().should == 1011
+        '千十一'.to_number.should == 1011
       end
       it "number to kanji 千六百" do
-        '千六百'.to_number().should == 1600
+        '千六百'.to_number.should == 1600
       end
       it "number to kanji 千六百七十" do
-        '千六百七十'.to_number().should == 1670
+        '千六百七十'.to_number.should == 1670
       end
       it "number to kanji 千六百七十九" do
-        '千六百七十九'.to_number().should == 1679
+        '千六百七十九'.to_number.should == 1679
       end
       it "number to kanji 万" do
-        '一万'.to_number().should == 10000
+        '一万'.to_number.should == 10000
       end
       it "number to kanji 万三千" do
-        '一万三千'.to_number().should == 13000
+        '一万三千'.to_number.should == 13000
       end
       it "number to kanji 六万" do
-        '六万'.to_number().should == 60000
+        '六万'.to_number.should == 60000
       end
       it "number to kanji 六万七千八百三十四" do
-        '六万七千八百三十四'.to_number().should == 67834
+        '六万七千八百三十四'.to_number.should == 67834
       end
       it "number to kanji 三千四百五十六" do
-        '一万二千三百四十五'.to_number().should == 12345
+        '一万二千三百四十五'.to_number.should == 12345
       end
       it "number to kanji 千二百三十四" do
-        '千二百三十四'.to_number().should == 1234
+        '千二百三十四'.to_number.should == 1234
       end
       it "number to kanji 十二万三千四百五十六6" do
-        '十二万三千四百五十六'.to_number().should == 123456
+        '十二万三千四百五十六'.to_number.should == 123456
       end
       it "number to kanji 百二十三万四千五百六十七" do
-        '百二十三万四千五百六十七'.to_number().should == 1234567
+        '百二十三万四千五百六十七'.to_number.should == 1234567
       end
       it "number to kanji 千二百三十四万五千六百七十八" do
-        '千二百三十四万五千六百七十八'.to_number().should == 12345678
+        '千二百三十四万五千六百七十八'.to_number.should == 12345678
       end
       it "number to kanji 八千七百六十五万四千三百二十一" do
-        '八千七百六十五万四千三百二十一'.to_number().should == 87654321
+        '八千七百六十五万四千三百二十一'.to_number.should == 87654321
       end
       it "number to kanji 一兆七千七百六十六億五千五百四十四万三千三百二十二" do
-        '一兆七千七百六十六億五千五百四十四万三千三百二十二'.to_number().should == 1776655443322
+        '一兆七千七百六十六億五千五百四十四万三千三百二十二'.to_number.should == 1776655443322
       end
       it "number to kanji 一億九千九百八十八万七千七百六十六" do
-        '一億九千九百八十八万七千七百六十六'.to_number().should == 199887766
+        '一億九千九百八十八万七千七百六十六'.to_number.should == 199887766
       end
       it "number to kanji 六百二十四兆七千八百二億四千二十万四千二百四" do
-        '六百二十四兆七千八百二億四千二十万四千二百四'.to_number().should == 624780240204204
+        '六百二十四兆七千八百二億四千二十万四千二百四'.to_number.should == 624780240204204
       end
       it "number to kanji 千百十一兆千百十一億千百十一万千百十一" do
-        '千百十一兆千百十一億千百十一万千百十一'.to_number().should == 1111111111111111
+        '千百十一兆千百十一億千百十一万千百十一'.to_number.should == ('1'*16).to_i
       end
       it "number to kanji 十億二千三十万四千五十" do
-        '十億二千三十万四千五十'.to_number().should == 1020304050
+        '十億二千三十万四千五十'.to_number.should == 1020304050
       end
       ### new
       it "number to kanji 一兆一" do
-        '一兆一'.to_number().should == 1000000000001
+        '一兆一'.to_number.should == ((10 ** 11).to_s + '1').to_i
       end
       it "number to kanji 京" do
-        '一京'.to_number().should == 10000000000000000
+        '一京'.to_number.should == 10 ** 16
       end
-
+      it "number to kanji 10 ^ 20" do
+        '一垓'.to_number.should == 10 ** 20
+      end
+      it "number to kanji 10 ^ 24" do
+        '一秭'.to_number.should == 10 ** 24
+      end
+      it "number to kanji 10 ^ 28" do
+        '一穣'.to_number.should == 10 ** 28
+      end
+      it "number to kanji 10 ^ 32" do
+        '一溝'.to_number.should == 10 ** 32
+      end
+      it "number to kanji 10 ^ 36" do
+        '一澗'.to_number.should == 10 ** 36
+      end
+      it "number to kanji 10 ^ 40" do
+        '一正'.to_number.should == 10 ** 40
+      end
+      it "number to kanji 10 ^ 44" do
+        '一載'.to_number.should == 10 ** 44
+      end
+      it "number to kanji 10 ^ 48" do
+        '一極'.to_number.should == 10 ** 48
+      end
+      it "number to kanji 一恒河沙" do
+        '一恒河沙'.to_number.should == 10 ** 52
+      end
+      it "number to kanji 七恒河沙千十極千十載千十正千十澗千十溝千十穣千十秭千十垓千十京千十兆千十億千十万千十" do
+        '七恒河沙千十極千十載千十正千十澗千十溝千十穣千十秭千十垓千十京千十兆千十億千十万千十'.to_number.should == ('7'+('10'*26)).to_i
+      end
+      it "number to kanji " do
+        '一阿僧祇'.to_number.should == (10 ** 56)
+      end
+      it "number to kanji '一阿僧祇千百二十二恒河沙三千三百四十四極'" do
+        '一阿僧祇千百二十二恒河沙三千三百四十四極'.to_number.should == ('111223344' +('0' * 48)).to_i
+      end
+      it "number to kanji '一那由他'" do
+        '一那由他'.to_number.should == (10 ** 60)
+      end
+      it "number to kanji '一不可思議'" do
+        '一不可思議'.to_number.should == (10 ** 64)
+      end
+      it "number to kanji 七不可思議千十那由他千十阿僧祇千十恒河沙千十極千十載千十正千十澗千十溝千十穣千十秭千十垓千十京千十兆千十億千十万千十" do
+        '七不可思議千十那由他千十阿僧祇千十恒河沙千十極千十載千十正千十澗千十溝千十穣千十秭千十垓千十京千十兆千十億千十万千十'.to_number.should == ('7'+('10'*32)).to_i
+      end
   end
 end
