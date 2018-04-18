fa6a6758e0d8a231017387baf5b63edf7a2127f5
QuiLT <quilt@atmarkcafe.org>
2018-04-18 10:16:19 +0700

    eo 37

--- lib/kansuji.rb
+++ lib/kansuji.rb
@@ -1,5 +1,4 @@
-KAN = {0=>"零", 1=>"一", 2=>"二", 3=>"三", 4=>"四", 5=>"五",6=>"六", 7=>"七", 8=>"八",
-      9=>"九", 10=>"十", (10**2)=>"百", (10 **3)=>"千", (10**4)=>"万", (10**8)=>"億",
+KAN = {0=>"零", 1=>"一", 2=>"二", 3=>"三", 4=>"四", 5=>"五",6=>"六", 7=>"七", 8=>"八",9=>"九", 10=>"十", (10**2)=>"百", (10 **3)=>"千", (10**4)=>"万", (10**8)=>"億",
       (10**12)=>"兆", (10**16)=>"京", (10**20)=>"垓", (10**24)=>"𥝱", (10**28)=>"穣",
       (10**32)=>"溝", (10**36)=>"澗", (10**40)=>"正", (10**44)=>"載", (10**48)=>"極",
       (10**52)=>"恒河沙", (10**56)=>"阿僧祇", (10**60)=>"那由他",  (10**64)=>"不可思議",
@@ -29,17 +28,14 @@ class Numeric
     return KAN[self] if self == 0
     self.to_s.reverse.split('').each_slice(4).with_index().each do |fa, index|
       key = (index > 0) ? KEY[index-1] : '' if fa.reverse.join('').to_i > 0
-      res = (kanji_to_demo(fa.reverse.join('')) + key.to_s + res)
+      res = kanji_to(fa.reverse.join('')) + key.to_s + res
     end
     return res
   end
-  def kanji_to_demo(str, kan = '')
-    begin
-      return str[0]=='0' ? kan : kan + KAN[str[0].to_i] if str.length==1
-      next if str[0]=='0'
-      next kan += KAN[str[0].to_i] + KAN[(10**(str.length-1))] if str[0] != '1'
-      kan = '一' + kan if (str.length >= 5)
-      kan += (KAN[(str[0].to_i * 10**(str.length-1))])
-    end while str.slice!(0).present?
+  def kanji_to(str, kan = '')
+    return str[0]=='0' ? '' : KAN[str[0].to_i] if str.length == 1
+    return kanji_to(str[1..(str.length)], kan = '') if str[0]=='0'
+    return KAN[str.slice!(0).to_i] + KAN[(10**str.length)] + kanji_to(str) if str[0] != '1'
+    return KAN[(str.slice!(0).to_i * 10**str.length)] + kanji_to(str, kan = '')
   end
 end
--- spec/lib/kansuji_to_spec.rb
+++ spec/lib/kansuji_to_spec.rb
@@ -61,110 +61,110 @@ describe Integer do
     # it "number to kanji 20000" do
     #   20000.to_kansuji.should == '二万'
     # end
-    it "number to kanji 13000" do
-      13000.to_kansuji.should == '一万三千'
-    end
-    it "number to kanji 60000" do
-      60000.to_kansuji.should == '六万'
-    end
+    # it "number to kanji 13000" do
+    #   13000.to_kansuji.should == '一万三千'
+    # end
+    # it "number to kanji 60000" do
+    #   60000.to_kansuji.should == '六万'
+    # end
     # it "number to kanji 10101" do
     #   10101.to_kansuji.should == '一万百一'
     # end
     it "number to kanji 123456" do
       123456.to_kansuji.should == '十二万三千四百五十六'
     end
-    # it "number to kanji 1234567" do
-    #   1234567.to_kansuji.should == '百二十三万四千五百六十七'
-    # end
-    # it "number to kanji 12345678" do
-    #   12345678.to_kansuji.should == '千二百三十四万五千六百七十八'
-    # end
-    # it "number to kanji 87654321" do
-    #   87654321.to_kansuji.should == '八千七百六十五万四千三百二十一'
-    # end
-    # it "number to kanji 1776655443322" do
-    #   1776655443322.to_kansuji.should == '一兆七千七百六十六億五千五百四十四万三千三百二十二'
-    # end
-    # it "number to kanji 199887766" do
-    #   199887766.to_kansuji.should == '一億九千九百八十八万七千七百六十六'
-    # end
-    # it "number to kanji 1020304050" do
-    #   1020304050.to_kansuji.should == '十億二千三十万四千五十'
-    # end
-    # it "number to kanji 1000000000001" do
-    #   1000000000001.to_kansuji.should == '一兆一'
-    # end
-    # it "number to kanji 10^16" do
-    #   (10 ** 16).to_kansuji.should == '一京'
-    # end
-    # it "number to kanji 10000000000007777" do
-    #   10000000000007777.to_kansuji.should == '一京七千七百七十七'
-    # end
-    # it "number to kanji 100032100000000000000" do
-    #   100032100000000000000.to_kansuji.should == '一垓三京二千百兆'
-    # end
-    # it "number to kanji 10^20" do
-    #   (10 **20).to_kansuji.should == '一垓'
-    # end
-    # it "number to kanji 10^24" do
-    #   (10 ** 24).to_kansuji.should == '一𥝱'
-    # end
-    # it "number to kanji 10^28" do
-    #   (10 ** 28).to_kansuji.should == '一穣'
-    # end
-    # it "number to kanji 10^32" do
-    #   (10 ** 32).to_kansuji.should == '一溝'
-    # end
-    # it "number to kanji 10^36" do
-    #   (10 ** 36).to_kansuji.should == '一澗'
-    # end
-    # it "number to kanji 10^40" do
-    #   (10 ** 40).to_kansuji.should == '一正'
-    # end
-    # it "number to kanji 10^44" do
-    #   (10 ** 44).to_kansuji.should == '一載'
-    # end
-    # it "number to kanji ('10123456'+ 10 ** 36)" do
-    #   ('10123456'+ (10 ** 36).to_s).to_i.to_kansuji.should == '一載百二十三正四千五百六十一澗'
-    # end
-    # it "number to kanji 1233524650345235465796552341233234" do
-    #   1233524650345235465796552341233234.to_kansuji.should == '十二溝三千三百五十二穣四千六百五十𥝱三千四百五十二垓三千五百四十六京五千七百九十六兆五千五百二十三億四千百二十三万三千二百三十四'
-    # end
-    # it "number to kanji 1234567890098765432112345678900987654321234567890" do
-    #   1234567890098765432112345678900987654321234567890.to_kansuji.should == '一極二千三百四十五載六千七百八十九正九十八澗七千六百五十四溝三千二百十一穣二千三百四十五𥝱六千七百八十九垓九十八京七千六百五十四兆三千二百十二億三千四百五十六万七千八百九十'
-    # end
-    # it "number to kanji 10 ^ 48" do
-    #   (10 ** 48).to_kansuji.should == '一極'
-    # end
-    # it "number to kanji 10 ^ 52" do
-    #   (10 ** 52).to_kansuji.should == '一恒河沙'
-    # end
-    # it "number to kanji ('7'+('10'*26))" do
-    #   ('7'+'10'*26).to_i.to_kansuji.should == '七恒河沙千十極千十載千十正千十澗千十溝千十穣千十𥝱千十垓千十京千十兆千十億千十万千十'
-    # end
-    # it "number to kanji 10 ^ 56" do
-    #   (10 ** 56).to_kansuji.should == '一阿僧祇'
-    # end
-    # it "number to kanji ('111223344' +('0' * 48))" do
-    #   ('111223344' +('0' * 48)).to_i.to_kansuji.should == '一阿僧祇千百二十二恒河沙三千三百四十四極'
-    # end
-    # it "number to kanji 10^60" do
-    #   (10 ** 60).to_kansuji.should == '一那由他'
-    # end
-    # it "number to kanji 10^60" do
-    #   (10 ** 64).to_kansuji.should == '一不可思議'
-    # end
-    # it "number to kanji ('7'+'10'*32)" do
-    #   ('7'+'10'*32).to_i.to_kansuji.should == '七不可思議千十那由他千十阿僧祇千十恒河沙千十極千十載千十正千十澗千十溝千十穣千十𥝱千十垓千十京千十兆千十億千十万千十'
-    # end
-    # it "number to kanji 1234567898765434567898765456" do
-    #   1234567898765434567898765456.to_kansuji.should == '千二百三十四𥝱五千六百七十八垓九千八百七十六京五千四百三十四兆五千六百七十八億九千八百七十六万五千四百五十六'
-    # end
-    # it "number to kanji 102030404000001112000211101010" do
-    #   102030404000001112000211101010.to_kansuji.should == '十穣二千三十𥝱四千四十垓千百十二兆二億千百十万千十'
-    # end
-    # it "number to kanji 10101020200202020303030404002202010101010" do
-    #   10101020200202020303030404002202010101010.to_kansuji.should == '一正百一澗二百二溝二十穣二千二十𥝱三千三十垓三千四十京四千二兆二千二十億千十万千十'
-    # end
+    it "number to kanji 1234567" do
+      1234567.to_kansuji.should == '百二十三万四千五百六十七'
+    end
+    it "number to kanji 12345678" do
+      12345678.to_kansuji.should == '千二百三十四万五千六百七十八'
+    end
+    it "number to kanji 87654321" do
+      87654321.to_kansuji.should == '八千七百六十五万四千三百二十一'
+    end
+    it "number to kanji 1776655443322" do
+      1776655443322.to_kansuji.should == '一兆七千七百六十六億五千五百四十四万三千三百二十二'
+    end
+    it "number to kanji 199887766" do
+      199887766.to_kansuji.should == '一億九千九百八十八万七千七百六十六'
+    end
+    it "number to kanji 1020304050" do
+      1020304050.to_kansuji.should == '十億二千三十万四千五十'
+    end
+    it "number to kanji 1000000000001" do
+      1000000000001.to_kansuji.should == '一兆一'
+    end
+    it "number to kanji 10^16" do
+      (10 ** 16).to_kansuji.should == '一京'
+    end
+    it "number to kanji 10000000000007777" do
+      10000000000007777.to_kansuji.should == '一京七千七百七十七'
+    end
+    it "number to kanji 100032100000000000000" do
+      100032100000000000000.to_kansuji.should == '一垓三京二千百兆'
+    end
+    it "number to kanji 10^20" do
+      (10 **20).to_kansuji.should == '一垓'
+    end
+    it "number to kanji 10^24" do
+      (10 ** 24).to_kansuji.should == '一𥝱'
+    end
+    it "number to kanji 10^28" do
+      (10 ** 28).to_kansuji.should == '一穣'
+    end
+    it "number to kanji 10^32" do
+      (10 ** 32).to_kansuji.should == '一溝'
+    end
+    it "number to kanji 10^36" do
+      (10 ** 36).to_kansuji.should == '一澗'
+    end
+    it "number to kanji 10^40" do
+      (10 ** 40).to_kansuji.should == '一正'
+    end
+    it "number to kanji 10^44" do
+      (10 ** 44).to_kansuji.should == '一載'
+    end
+    it "number to kanji ('10123456'+ 10 ** 36)" do
+      ('10123456'+ (10 ** 36).to_s).to_i.to_kansuji.should == '一載百二十三正四千五百六十一澗'
+    end
+    it "number to kanji 1233524650345235465796552341233234" do
+      1233524650345235465796552341233234.to_kansuji.should == '十二溝三千三百五十二穣四千六百五十𥝱三千四百五十二垓三千五百四十六京五千七百九十六兆五千五百二十三億四千百二十三万三千二百三十四'
+    end
+    it "number to kanji 1234567890098765432112345678900987654321234567890" do
+      1234567890098765432112345678900987654321234567890.to_kansuji.should == '一極二千三百四十五載六千七百八十九正九十八澗七千六百五十四溝三千二百十一穣二千三百四十五𥝱六千七百八十九垓九十八京七千六百五十四兆三千二百十二億三千四百五十六万七千八百九十'
+    end
+    it "number to kanji 10 ^ 48" do
+      (10 ** 48).to_kansuji.should == '一極'
+    end
+    it "number to kanji 10 ^ 52" do
+      (10 ** 52).to_kansuji.should == '一恒河沙'
+    end
+    it "number to kanji ('7'+('10'*26))" do
+      ('7'+'10'*26).to_i.to_kansuji.should == '七恒河沙千十極千十載千十正千十澗千十溝千十穣千十𥝱千十垓千十京千十兆千十億千十万千十'
+    end
+    it "number to kanji 10 ^ 56" do
+      (10 ** 56).to_kansuji.should == '一阿僧祇'
+    end
+    it "number to kanji ('111223344' +('0' * 48))" do
+      ('111223344' +('0' * 48)).to_i.to_kansuji.should == '一阿僧祇千百二十二恒河沙三千三百四十四極'
+    end
+    it "number to kanji 10^60" do
+      (10 ** 60).to_kansuji.should == '一那由他'
+    end
+    it "number to kanji 10^60" do
+      (10 ** 64).to_kansuji.should == '一不可思議'
+    end
+    it "number to kanji ('7'+'10'*32)" do
+      ('7'+'10'*32).to_i.to_kansuji.should == '七不可思議千十那由他千十阿僧祇千十恒河沙千十極千十載千十正千十澗千十溝千十穣千十𥝱千十垓千十京千十兆千十億千十万千十'
+    end
+    it "number to kanji 1234567898765434567898765456" do
+      1234567898765434567898765456.to_kansuji.should == '千二百三十四𥝱五千六百七十八垓九千八百七十六京五千四百三十四兆五千六百七十八億九千八百七十六万五千四百五十六'
+    end
+    it "number to kanji 102030404000001112000211101010" do
+      102030404000001112000211101010.to_kansuji.should == '十穣二千三十𥝱四千四十垓千百十二兆二億千百十万千十'
+    end
+    it "number to kanji 10101020200202020303030404002202010101010" do
+      10101020200202020303030404002202010101010.to_kansuji.should == '一正百一澗二百二溝二十穣二千二十𥝱三千三十垓三千四十京四千二兆二千二十億千十万千十'
+    end
   end
 end
