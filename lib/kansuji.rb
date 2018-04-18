64d6fcfacfcf7cfa2e28589fcc7d0e34d3d7e002
QuiLT <quilt@atmarkcafe.org>
2018-04-18 11:06:44 +0700

    refactor

--- lib/kansuji.rb
+++ lib/kansuji.rb
@@ -6,21 +6,20 @@ KAN = {0=>"零", 1=>"一", 2=>"二", 3=>"三", 4=>"四", 5=>"五",6=>"六", 7=>"
 KEY = ['万','億','兆','京','垓','𥝱','穣','溝','澗','正','載','極','恒河沙','阿僧祇','那由他','不可思議','無量大数']
 class String
   def to_number(res = 0, curr = 0)
-    return get_number(self) if !KEY.any?{ |key| self.include?(key) }
+    return get_number_to(self) if !KEY.any?{ |key| self.include?(key) }
     KEY.reverse.each do |item|
       next if self.index(item).blank?
-      res += get_number(self[curr..self.index(item)-1]) * KAN.key(item)
+      res += get_number_to(self[curr..self.index(item)-1]) * KAN.key(item)
       curr = self.index(item) + item.length
     end
-    return res + get_number(self[curr..self.length]).to_i
+    return res + get_number_to(self[curr..self.length]).to_i
   end
-  def get_number(str, res = 0, curr = '')
-    begin
-       curr = KAN.key(str[0]).to_i
-       curr *= KAN.key(str.slice!(1..1)).to_i if (curr % 10 != 0 &&
-               KAN.key(str[1]).to_i % 10 == 0 && str[1].present?)
-       (str.length == 1) ? (return res + curr) : res += curr
-     end while str.slice!(0).present?
+  def get_number_to(str, res = 0, curr = '')
+    curr = KAN.key(str.slice!(0)).to_i
+    curr *= KAN.key(str.slice!(0)).to_i if (curr % 10 != 0 &&
+             KAN.key(str[0]).to_i % 10 == 0 && str[0].present?)
+    return curr if str.length == 0
+    return curr + get_number_to(str, res = 0, curr = '')
   end
 end
 class Numeric
@@ -33,7 +32,7 @@ class Numeric
     return res
   end
   def kanji_to(str, kan = '')
-    return str[0]=='0' ? '' : KAN[str[0].to_i] if str.length == 1
+    return str[0] == '0' ? '' : KAN[str[0].to_i] if str.length == 1
     return kanji_to(str[1..(str.length)], kan = '') if str[0]=='0'
     return KAN[str.slice!(0).to_i] + KAN[(10**str.length)] + kanji_to(str) if str[0] != '1'
     return KAN[(str.slice!(0).to_i * 10**str.length)] + kanji_to(str, kan = '')
